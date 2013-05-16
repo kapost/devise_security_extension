6b58b3909713c1d60def8d2ff5d52510ea9ac7f4
John LeBoeuf-Little <john@kapost.com>
2013-05-16 14:50:45 -0600

    Require password expiration only if advanced security is enabled.

--- lib/devise_security_extension/hooks/password_expirable.rb
+++ lib/devise_security_extension/hooks/password_expirable.rb
@@ -1,12 +1,5 @@
 Warden::Manager.after_authentication do |record, warden, options|
   if record.respond_to?(:need_change_password?)
-    puts "===================================================================================================================================="
-    puts record
-    puts warden
-    puts options
-    puts record.need_change_password?
-    puts warden.session(options[:scope])[:password_expired]
-    warden.session(options[:scope])[:password_expired] = record.need_change_password?
-    puts "===================================================================================================================================="
+    warden.session(options[:scope])[:password_expired] = record.need_change_password? and record.advanced_security_required?
   end
 end
