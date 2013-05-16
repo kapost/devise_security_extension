89d892c9656503fe44012a1331597fbce4566807
John LeBoeuf-Little <john@kapost.com>
2013-05-16 13:01:41 -0600

    Debugging.

--- lib/devise_security_extension/hooks/password_expirable.rb
+++ lib/devise_security_extension/hooks/password_expirable.rb
@@ -6,7 +6,7 @@ Warden::Manager.after_authentication do |record, warden, options|
     puts options
     puts record.need_change_password?
     puts warden.session(options[:scope])[:password_expired]
-    # warden.session(options[:scope])[:password_expired] = record.need_change_password?
+    warden.session(options[:scope])[:password_expired] = record.need_change_password?
     puts "===================================================================================================================================="
   end
 end
